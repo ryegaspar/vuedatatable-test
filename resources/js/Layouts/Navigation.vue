fed8f66cb64bcec32b0b71db563328d29d3069e3
rye gaspar <gaspar.ryan@outlook.com>
2022-08-06 16:40:31 -0500

    more nav tweak

--- resources/js/Layouts/Navigation.vue
+++ resources/js/Layouts/Navigation.vue
@@ -1,30 +1,29 @@
 <script setup>
-import {ref} from 'vue'
 import {Link} from "@inertiajs/inertia-vue3"
 
-let pages = ref(
-	[
-		{
-			link: '01',
-			title: 'Basic'
-		},
-		{
-			link: '02',
-			title: 'Advance'
-		},
-	]
-)
+let pages = [
+	{
+		link: '01',
+		title: 'Basic'
+	},
+	{
+		link: '02',
+		title: 'Advance'
+	},
+]
+
 </script>
+
 <template>
 	<div id="app" class="font-sans antialiased text-black leading-tight">
 		<div class="flex flex-col h-screen">
 			<div class="bg-indigo-800 px-6 py-4 relative"></div>
 			<div class="flex flex-1 bg-gray-50">
 				<div class="flex flex-col flex-none overflow-hidden border-r-2 w-64 lg:max-w-xs max-h-screen">
-					<ol class="list-reset overflow-y-auto">
+					<ol class="list-reset overflow-y-auto mt-4">
 						<li v-for="page in pages">
 							<Link :href="route(page.link)"
-									   class="flex justify-between items-baseline no-underline p-2 hover:bg-blue-300"
+								  class="flex justify-between items-baseline no-underline p-2 hover:bg-blue-300"
 							>
 								<div class="flex items-baseline text-sm mr-4 relative">
 									<span class="pl-2 text-gray-800">{{ page.title }}</span>
