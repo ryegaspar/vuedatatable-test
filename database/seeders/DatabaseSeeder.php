2fae14a76511aca702572eaff4d5aa11e719657c
rye gaspar <gaspar.ryan@outlook.com>
2022-08-06 10:03:08 -0500

    migration and inertia

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
+use App\Models\Product;
+use App\Models\Shop;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(100)->create();
+        Shop::factory(5)->create();
+        Product::factory(20)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Shop::find(1)->products()->attach(Product::find(1));
+        Shop::find(1)->products()->attach(Product::find(2));
+
+		Post::factory(100)->create([
+			'user_id' => User::find(1)
+		]);
     }
 }
