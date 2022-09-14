16b87eb8a2ccfd55f60375aa82782f10343e14b9
rye gaspar <gaspar.ryan@outlook.com>
2022-09-13 20:28:53 -0500

    add another table on page 1 with different field and css

--- resources/js/Pages/01.vue
+++ resources/js/Pages/01.vue
@@ -1,6 +1,6 @@
 <script setup>
-let apiUrl = '/users'
-let fields = [
+const apiUrl = '/users'
+const fields = [
 	{
 		name: 'id',
 		title: 'ID',
@@ -13,6 +13,29 @@ let fields = [
 		name: 'email',
 		title: 'Email',
 		titleClass: 'text-center text-red-600 bg-gray-400'
+	},
+	{
+		name: 'nickname',
+		title: 'Nickname',
+	},
+	{
+		name: 'birthdate',
+		title: 'Birthdate'
+	}
+]
+
+const fields2 = [
+	{
+		name: 'id',
+		title: 'ID',
+	},
+	{
+		name: 'name',
+		title: 'Name',
+	},
+	{
+		name: 'email',
+		title: 'Email',
 	},
 	{
 		name: 'nickname',
@@ -37,6 +60,12 @@ let fields = [
 		<vue-datatable :api-url="apiUrl"
 					   :fields="fields"
 					   :styles="{tableHeaderClass: 'bg-blue-400'}"
+		/>
+	</div>
+
+	<div class="mt-2 py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+		<vue-datatable :api-url="apiUrl"
+					   :fields="fields2"
 		/>
 	</div>
 </template>
