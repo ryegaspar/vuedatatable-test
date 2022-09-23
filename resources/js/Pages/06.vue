8dde993a1342d1161172779e50993f851f93e9b6
rye gaspar <gaspar.ryan@outlook.com>
2022-09-22 22:12:56 -0500

    page 6 experimental loading

--- resources/js/Pages/06.vue
+++ resources/js/Pages/06.vue
@@ -54,13 +54,12 @@ const defaultCss = {
 // handleIcon: 'grey sidebar icon',
 // tableBodyClass: ''
 // detailRowClass: 'vdatatable-detail-row',
-
 </script>
 
 <template>
 	<div>
 		<div class="-mx-6 text-sm shadow sm:-mx-6 md:mx-0 md:overflow-hidden md:rounded-lg"><!-- tableWrapperClass -->
-			<table class="min-w-full divide-y divide-gray-300"><!-- tableClass-->
+			<table class="loading min-w-full divide-y divide-gray-300"><!-- tableClass-->
 				<thead class="bg-gray-800 text-gray-100"><!-- tableHeaderClass -->
 				<tr>
 					<!-- *tableHeaderCellClass, *tableHeaderClassSortable -->
@@ -289,4 +288,20 @@ const defaultCss = {
 			</div>
 		</nav>
 	</div>
+
+	<!-- loading SVG!!!
+	<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" id='loadingSvg'><style type='text/css'> #loadingSvg{animation:spin 1s linear infinite}@keyframes spin{from{transform:rotate(0deg)}to{transform:rotate(360deg)}}</style><circle class="opacity-25" cx="12" cy="12" r="10" stroke="#9CA3AF" stroke-width="4"></circle><path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>
+	-->
 </template>
+<style>
+
+table.loading tbody {
+	@apply relative
+}
+
+table.loading tbody:after {
+	@apply absolute inset-0 bg-gray-700/25 bg-center bg-no-repeat backdrop-blur-sm content-[''];
+	background-size: 50px 50px;
+	background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' id='loadingSvg'%3E%3Cstyle type='text/css'%3E %23loadingSvg%7Banimation:spin 1s linear infinite%7D@keyframes spin%7Bfrom%7Btransform:rotate(0deg)%7Dto%7Btransform:rotate(360deg)%7D%7D%3C/style%3E%3Ccircle style='opacity:0.25' cx='12' cy='12' r='10' stroke='%23374151' stroke-width='4'%3E%3C/circle%3E%3Cpath style='opacity:0.75' fill='%231F2937' d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'%3E%3C/path%3E%3C/svg%3E");
+}
+</style>
